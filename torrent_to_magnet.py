f3f5709435d45594cd048bad2c106cf3f65798f7
Romanbot4 <tice.lover1500@gmail.com>
2021-12-13 00:22:47 +0630

    refactored the length, name extraction

--- torrent_to_magnet.py
+++ torrent_to_magnet.py
@@ -3,19 +3,31 @@ import bencodepy
 import hashlib
 import base64
 from os.path import exists
+import urllib.request
 
-#download a file and return the local path
 def download_file(url, file):
-    import urllib.request
     urllib.request.urlretrieve(url, file)
     return file
 
-# check whether a given link is local or network
 def is_local(link):
     if link.startswith('http'):
         return False
     else:
         return True
+
+def _get_length(subj):
+    if b'length' in subj:
+        return subj[b'length']
+    elif b'files' in subj:
+        return sum([_get_length(f[b'path']) for f in subj[b'files']])
+    else:
+        return 0
+
+def _get_name(subj):
+    if b'name' in subj:
+        return subj[b'name']
+    else:
+        return ''
 
 def make_magnet_from_file(file) :
     metadata = bencodepy.decode_from_file(file)
@@ -23,9 +35,9 @@ def make_magnet_from_file(file) :
     hashcontents = bencodepy.encode(subj)
     digest = hashlib.sha1(hashcontents).digest()
     b32hash = base64.b32encode(digest)
-    name = subj[b'name']
+    name = _get_name(subj)
     announce = metadata[b'announce']
-    length = subj[b'files'][0][b'length']
+    length = _get_length(subj)
     return 'magnet:?'\
              + 'xt=urn:btih:' + b32hash.decode()\
              + '&dn=' + name.decode()\
@@ -39,6 +51,6 @@ def make_magnet_from_torrent(torrent):
     else:
         return make_magnet_from_file(download_file(torrent, 'temp.torrent'))
 
-link  = 'C:/Users/ticel/Downloads/onejav.com_abw178.torrent'
+link  = 'https://releases.ubuntu.com/21.10/ubuntu-21.10-desktop-amd64.iso.torrent?_ga=2.201817617.1652849701.1639329806-1115114217.1639329806'
 string = make_magnet_from_torrent(link)
 print(string)
